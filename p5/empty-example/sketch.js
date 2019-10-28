2950128354426a64130ce7e34886e9d5a221ae62
BrianAllison-kruel <55714245+BrianAllison-kruel@users.noreply.github.com>
2019-10-28 10:42:05 -0700

    update

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,168 +1,131 @@
 function setup() {
- createCanvas(1000, 1000);
+ createCanvas(10000, 10000);
 }
 
-// the snake is divided into small segments, which are drawn and edited on each 'draw' call
-let numSegments = 10;
-let direction = 'right';
-
-const xStart = 0; //starting x coordinate for snake
-const yStart = 250; //starting y coordinate for snake
-const diff = 10;
-
-let xCor = [];
-let yCor = [];
-
-let xFruit = 0;
-let yFruit = 0;
-let scoreElem;
+// All the paths
+let paths = [];
+// Are we painting?
+let painting = false;
+// How long until the next circle
+let next = 0;
+// Where are we now and where were we?
+let current;
+let previous;
 
 function setup() {
-  scoreElem = createDiv('Score = 0');
-  scoreElem.position(20, 20);
-  scoreElem.id = 'score';
-  scoreElem.style('color', 'white');
-
-  createCanvas(500, 500);
-  frameRate(15);
-  stroke(255);
-  strokeWeight(10);
-  updateFruitCoordinates();
-
-  for (let i = 0; i < numSegments; i++) {
-    xCor.push(xStart + i * diff);
-    yCor.push(yStart);
-  }
-}
+  createCanvas(720, 400);
+  current = createVector(0,0);
+  previous = createVector(0,0);
+};
 
 function draw() {
-  background(0);
-  for (let i = 0; i < numSegments - 1; i++) {
-    line(xCor[i], yCor[i], xCor[i + 1], yCor[i + 1]);
-  }
-  updateSnakeCoordinates();
-  checkGameStatus();
-  checkForFruit();
-}
+  background(200);
+  
+  // If it's time for a new point
+  if (millis() > next && painting) {
 
-/*
- The segments are updated based on the direction of the snake.
- All segments from 0 to n-1 are just copied over to 1 till n, i.e. segment 0
- gets the value of segment 1, segment 1 gets the value of segment 2, and so on,
- and this results in the movement of the snake.
+    // Grab mouse position      
+    current.x = mouseX;
+    current.y = mouseY;
 
- The last segment is added based on the direction in which the snake is going,
- if it's going left or right, the last segment's x coordinate is increased by a
- predefined value 'diff' than its second to last segment. And if it's going up
- or down, the segment's y coordinate is affected.
-*/
-function updateSnakeCoordinates() {
-  for (let i = 0; i < numSegments - 1; i++) {
-    xCor[i] = xCor[i + 1];
-    yCor[i] = yCor[i + 1];
+    // New particle's force is based on mouse movement
+    let force = p5.Vector.sub(current, previous);
+    force.mult(0.05);
+
+    // Add new particle
+    paths[paths.length - 1].add(current, force);
+    
+    // Schedule next circle
+    next = millis() + random(100);
+
+    // Store mouse values
+    previous.x = current.x;
+    previous.y = current.y;
   }
-  switch (direction) {
-    case 'right':
-      xCor[numSegments - 1] = xCor[numSegments - 2] + diff;
-      yCor[numSegments - 1] = yCor[numSegments - 2];
-      break;
-    case 'up':
-      xCor[numSegments - 1] = xCor[numSegments - 2];
-      yCor[numSegments - 1] = yCor[numSegments - 2] - diff;
-      break;
-    case 'left':
-      xCor[numSegments - 1] = xCor[numSegments - 2] - diff;
-      yCor[numSegments - 1] = yCor[numSegments - 2];
-      break;
-    case 'down':
-      xCor[numSegments - 1] = xCor[numSegments - 2];
-      yCor[numSegments - 1] = yCor[numSegments - 2] + diff;
-      break;
+
+  // Draw all paths
+  for( let i = 0; i < paths.length; i++) {
+    paths[i].update();
+    paths[i].display();
   }
 }
 
-/*
- I always check the snake's head position xCor[xCor.length - 1] and
- yCor[yCor.length - 1] to see if it touches the game's boundaries
- or if the snake hits itself.
-*/
-function checkGameStatus() {
-  if (
-    xCor[xCor.length - 1] > width ||
-    xCor[xCor.length - 1] < 0 ||
-    yCor[yCor.length - 1] > height ||
-    yCor[yCor.length - 1] < 0 ||
-    checkSnakeCollision()
-  ) {
-    noLoop();
-    const scoreVal = parseInt(scoreElem.html().substring(8));
-    scoreElem.html('Game ended! Your score was : ' + scoreVal);
-  }
+// Start it up
+function mousePressed() {
+  next = 0;
+  painting = true;
+  previous.x = mouseX;
+  previous.y = mouseY;
+  paths.push(new Path());
 }
 
-/*
- If the snake hits itself, that means the snake head's (x,y) coordinate
- has to be the same as one of its own segment's (x,y) coordinate.
-*/
-function checkSnakeCollision() {
-  const snakeHeadX = xCor[xCor.length - 1];
-  const snakeHeadY = yCor[yCor.length - 1];
-  for (let i = 0; i < xCor.length - 1; i++) {
-    if (xCor[i] === snakeHeadX && yCor[i] === snakeHeadY) {
-      return true;
+// Stop
+function mouseReleased() {
+  painting = false;
+}
+
+// A Path is a list of particles
+class Path {
+  constructor() {
+    this.particles = [];
+    this.hue = random(100);
+  }
+
+  add(position, force) {
+    // Add a new particle with a position, force, and hue
+    this.particles.push(new Particle(position, force, this.hue));
+  }
+  
+  // Display plath
+  update() {  
+    for (let i = 0; i < this.particles.length; i++) {
+      this.particles[i].update();
     }
+  }  
+  
+  // Display plath
+  display() {    
+    // Loop through backwards
+    for (let i = this.particles.length - 1; i >= 0; i--) {
+      // If we shold remove it
+      if (this.particles[i].lifespan <= 0) {
+        this.particles.splice(i, 1);
+      // Otherwise, display it
+      } else {
+        this.particles[i].display(this.particles[i+1]);
+      }
+    }
+  
+  }  
+}
+
+// Particles along the path
+class Particle {
+  constructor(position, force, hue) {
+    this.position = createVector(position.x, position.y);
+    this.velocity = createVector(force.x, force.y);
+    this.drag = 0.95;
+    this.lifespan = 255;
   }
-}
 
-/*
- Whenever the snake consumes a fruit, I increment the number of segments,
- and just insert the tail segment again at the start of the array (basically
- I add the last segment again at the tail, thereby extending the tail)
-*/
-function checkForFruit() {
-  point(xFruit, yFruit);
-  if (xCor[xCor.length - 1] === xFruit && yCor[yCor.length - 1] === yFruit) {
-    const prevScore = parseInt(scoreElem.html().substring(8));
-    scoreElem.html('Score = ' + (prevScore + 1));
-    xCor.unshift(xCor[0]);
-    yCor.unshift(yCor[0]);
-    numSegments++;
-    updateFruitCoordinates();
+  update() {
+    // Move it
+    this.position.add(this.velocity);
+    // Slow it down
+    this.velocity.mult(this.drag);
+    // Fade it out
+    this.lifespan--;
   }
-}
 
-function updateFruitCoordinates() {
-  /*
-    The complex math logic is because I wanted the point to lie
-    in between 100 and width-100, and be rounded off to the nearest
-    number divisible by 10, since I move the snake in multiples of 10.
-  */
-
-  xFruit = floor(random(10, (width - 100) / 10)) * 10;
-  yFruit = floor(random(10, (height - 100) / 10)) * 10;
-}
-
-function keyPressed() {
-  switch (keyCode) {
-    case 74:
-      if (direction !== 'right') {
-        direction = 'left';
-      }
-      break;
-    case 76:
-      if (direction !== 'left') {
-        direction = 'right';
-      }
-      break;
-    case 73:
-      if (direction !== 'down') {
-        direction = 'up';
-      }
-      break;
-    case 75:
-      if (direction !== 'up') {
-        direction = 'down';
-      }
-      break;
+  // Draw particle and connect it with a line
+  // Draw a line to another
+  display(other) {
+    stroke(0, this.lifespan);
+    fill(0, this.lifespan/2);    
+    ellipse(this.position.x,this.position.y, 8, 8);    
+    // If we need to draw a line
+    if (other) {
+      line(this.position.x, this.position.y, other.position.x, other.position.y);
+    }
   }
 }
