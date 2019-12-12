ed8f69784717e1013bd4f4b64dcbb2690fb9b11f
BrianAllison-kruel <55714245+BrianAllison-kruel@users.noreply.github.com>
2019-12-11 18:06:33 -0800

    updated

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -10,4 +10,23 @@ function draw()
 }
 
 
+function draw() 
+{  background(204, 226, 225); 
+    fill(255, 0, 0, 160);  
+    ellipse(132, 82, 200, 200);
+    fill(0, 255, 0, 160); 
+    ellipse(228, -16, 200, 200); 
+    fill(0, 0, 255, 160); 
+    ellipse(268, 118, 200, 200);  
+}
+
+function draw()
+{
+    if (MouseisPressed)
+    { fill (0, 160, 255, 0); } else
+    { triangle(158, 55, 290, 91, 290, 112); }
+    { fill(0, 255); }
+    ellipse(mouseX, mouseY, 50, 100);
+}
+
 
